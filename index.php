c854a6acaa1fb6684d6ece1c9794c015f4d97ced
iruiwei <banda_rui@hotmail.com>
2012-09-20 17:04:41 +0900

    add new files
    database

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+ require('dbconnect.php');
+ $sql1=sprintf('select count(*) as cnt from mb_restaurant');
+ $record=mysql_query($sql1)or die(mysql_error());
+ $table=mysql_fetch_assoc($record);
+ if($table['cnt']==0){
+		echo '0!!';
+	}
+	else{
+		echo 'Welcome!!';
+	}
+?>
