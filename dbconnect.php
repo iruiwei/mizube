e8df3b70a8f8df8c29e3ab6eee8a430394634616
iruiwei <nogamijoe@gmail.com>
2012-09-21 20:43:33 +0900

    config changed

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-mysql_connect('mysql508.phy.lolipop.jp','LA05918484','CPNMOUVt') or die(mysql_error());
-mysql_select_db('LA05918484-ownjd6') or die(mysql_error());
+mysql_connect('localhost','root','root') or die(mysql_error());
+mysql_select_db('database_name') or die(mysql_error());
 mysql_query('SET NAMES UTF8');
 ?>
