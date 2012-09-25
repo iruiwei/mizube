82f0ea4ea3fc48c0bb6aa897b1b175e58b220a49
iruiwei <banda_rui@hotmail.com>
2012-09-25 11:31:57 +0900

    ルート検索完成
    制限条件なし（時間とチケット）

--- area_result.php
+++ area_result.php
@@ -33,8 +33,53 @@ if(!isset($_POST)){
 	$m=$_POST['mm'];
 	$s=$_POST['ss'];
 	$t=mktime("$h","$m","$s");
-	echo $lev.$arv.$sel.$h.$m.$s.$t;
-	//echo $SESSION['a_result']['a_leave'];
+	$sql1=sprintf('select count(*) as cnt from mb_shiptime where departureID="%d" and arrivalID="%d"',mysql_real_escape_string($lev),mysql_real_escape_string($arv));
+	$recordset1=mysql_query($sql1)or die(mysql_error());
+	$data1=mysql_fetch_assoc($recordset1));
+	if($data1['cnt']>0){
+		$sql=sprintf('select * from mb_shiptime where departureID="%d" and arrivalID="%d"',mysql_real_escape_string($lev),mysql_real_escape_string($arv));
+		$table=mysql_query($sql)or die(mysql_error());
+		while($data=mysql_fetch_assoc($table)){
+			echo $data['departureID']." ";
+			echo $data['arrivalID']." ";
+			echo '<br />';
+		}
+	}
+	else{
+		$sql2=sprintf('select count(*) as cnt from mb_shiptime as t1 right join mb_shiptime as t2 on t1.arrivalID=t2.departureID where t1.departureID="%d" and t2.arrivalID="%d" and t1.departureID<>t2.arrivalID',mysql_real_escape_string($lev),mysql_real_escape_string($arv));
+		$recordset2=mysql_query($sql2)or die(mysql_error());
+		$data2=mysql_fetch_assoc($recordset2));
+		if($data2['cnt']>0){
+			$sql=sprintf('select t1.departureID as lev,t1.arrivalID as arv1,t2.arrivalID as arv2 from mb_shiptime as t1 right join mb_shiptime as t2 on t1.arrivalID=t2.departureID where t1.departureID="%d" and t2.arrivalID="%d" and t1.departureID<>t2.arrivalID' , mysql_real_escape_string($lev), mysql_real_escape_string($arv));
+			$table=mysql_query($sql)or die(mysql_error());
+			while($data=mysql_fetch_assoc($table)){
+				echo $data['lev']." ";
+				echo $data['arv1']." ";
+				echo $data['arv2']." ";
+				echo '<br />';
+			}
+		}
+		else{
+			$sql3=sprintf('select count(*) as cnt from (select r1.departureID c1,r1.arrivalID c2,r2.arrivalID c3 from mb_shiptime r1 inner join mb_shiptime r2 on r1.arrivalID=r2.departureID where r1.departureID="%d") r3 inner join mb_shiptime r4 on r3.c3=r4.departureID where r4.arrivalID="%d"',mysql_real_escape_string($lev),mysql_real_escape_string($arv));
+			$recordset3=mysql_query($sql3)or die(mysql_error());
+			$data3=mysql_fetch_assoc($recordset3));
+			if($data2['cnt']>0){
+				$sql=sprintf('select r3.c1,r3.c2,r3.c3,r4.arrivalID c4 from (select r1.departureID c1,r1.arrivalID c2,r2.arrivalID c3 from mb_shiptime r1 inner join mb_shiptime r2 on r1.arrivalID=r2.departureID where r1.departureID="%d") r3 inner join mb_shiptime r4 on r3.c3=r4.departureID where r4.arrivalID="%d"' , mysql_real_escape_string($lev), mysql_real_escape_string($arv));
+				$table=mysql_query($sql)or die(mysql_error());
+				while($data=mysql_fetch_assoc($table)){
+					echo $data['c1']." ";
+					echo $data['c2']." ";
+					echo $data['c3']." ";
+					echo $data['c4']." ";
+					echo '<br />';
+				}
+			}
+			else{
+				echo "No Result!";
+			}
+		}
+	}
+	//echo $lev.$arv.$sel.$h.$m.$s.$t;
 	?>
 
 </body>
