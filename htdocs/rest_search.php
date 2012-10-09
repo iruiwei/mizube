de6faa87c1b928754e71333f6f8d98da5b1f6abd
ttyymmt <tetsuya.y@gmail.com>
2012-10-10 00:23:18 +0900

    added google analytics

--- htdocs/rest_search.php
+++ htdocs/rest_search.php
@@ -14,6 +14,19 @@ require('dbconnect.php');
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes">
 	<link rel="stylesheet" href="style.css">
 	<!-- Date: 2012-10-08 -->
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35428958-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 	<header>
--- htdocs/ship.php
+++ htdocs/ship.php
@@ -39,6 +39,19 @@ else $d=3;
 		<a href="index.html"><img src= "img/logo.png" style="width:100%"></a>
 		<img src= "img/line.png" style="width:100%">
 		</div>
+		<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35428958-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 	</header>
 	<div id="info">
 		エリアを選んでください。
